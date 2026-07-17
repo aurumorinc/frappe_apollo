d20d017c70f8d7890405bb68948cd8d30969f9df
Aryan Singh <aquiveal@gmail.com>
2026-07-17 19:33:25 +0530

    feat: add ApolloCadenceProvider class
    Implement ApolloCadenceProvider as a subclass of BaseCadenceProvider
    to handle Apollo-specific cadence event reporting. Move the enqueue
    import to the point of use within on_update function to reduce module
    dependencies at import time.

--- frappe_apollo/apollo/doctype/cadence_provider/cadence_provider.py
+++ frappe_apollo/apollo/doctype/cadence_provider/cadence_provider.py
@@ -1,12 +1,18 @@
 import frappe
-from frappe_controller.utils.background_jobs import enqueue
+from frappe_cadence.cadence.doctype.cadence_provider.cadence_provider import BaseCadenceProvider
+
+class ApolloCadenceProvider(BaseCadenceProvider):
+	@classmethod
+	def report_event(cls, event_type: str, context: dict, data: dict = None):
+		pass
 
 def on_update(doc, method=None):
-	# If Apollo settings change, all sequences need to self-hydrate
+	# If Apollo provider settings change, all sequences need to self-hydrate
 	if doc.name == "Apollo":
 		# Only enqueue for cadences linked to sequences
 		sequences = frappe.get_all("Sequence", pluck="cadence")
 		unique_cadences = list(set(sequences))
+		from frappe_controller.utils.background_jobs import enqueue
 		for cadence_name in unique_cadences:
 			enqueue(
 				method="frappe_apollo.apollo.doctype.sequence.sequence.on_cadence_update",
